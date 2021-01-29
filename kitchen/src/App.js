28c6def5096ba2be25d5759aaac8832011a0ee56
Jacqueline-Jane Denney <jackiecdenney@gmail.com>
2021-01-29 14:11:45 -0500

    put in an autoscroll componenet. testing now. '

--- kitchen/src/App.js
+++ kitchen/src/App.js
@@ -1,11 +1,18 @@
 
 import Images from './React-Grid-Gallery/Images'
 import Body from './Layout/Body'
+import Footer from './Layout/Footer'
+import AutoScroll from './AutoScroll/AutoScroll'
 function App() {
   return (
-    <Body>
-      <Images></Images>
-    </Body>
+    <>
+      <Body>
+        <Images></Images>
+      </Body>
+      <Footer>
+        <AutoScroll></AutoScroll>
+      </Footer>
+    </>
   )
 }
 
---
+++ kitchen/src/AutoScroll/AutoScroll.js
@@ -0,0 +1,28 @@
+
+import React from 'React'
+import ReactAutoScroll from "react-to-target-auto-scroll"
+
+export default class AutoScroll extends Component {
+  render() {
+    return (
+      <ReactAutoScroll
+        targetPosition={1}
+        easeType={'linear'}
+        speed={7}
+        updateInterval={40}
+        onScrollingDone={() => console.log('scrolling finished')}
+        scrollTargetRef={this.refs.scrollExample}
+        isEnabled
+      >
+        <div
+          ref="scrollExample"
+          style={{ overflow: 'scroll', backgroundColor: 'red', width: '300px', height: '300px' }}
+          >
+           <div style={{ height: '2000px' }}>
+            content
+           </div>
+         </div>
+     </ReactAutoScroll>
+    );
+  }
+}
