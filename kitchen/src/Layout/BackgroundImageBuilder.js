41c98703e18953546b9d45d574682da89e67b100
Jacqueline-Jane Denney <jackiecdenney@gmail.com>
2021-01-30 14:17:48 -0500

    changed color of background

---
+++ kitchen/src/Layout/BackgroundImageBuilder.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Axios from 'axios'
+
+class BackgroundImageBuilder extends Component {
+  state = {
+    backgroundImgArray =[]
+  }
+  componentDidMount() {
+    
+  }
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+BackgroundImageBuilder.propTypes = {
+
+};
+
+export default BackgroundImageBuilder;
