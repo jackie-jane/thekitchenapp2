51ac023a00e8f3833a8699cda670dd1c6550ddb6
Jacqueline-Jane Denney <jackiecdenney@gmail.com>
2021-02-02 16:06:47 -0500

    onclick function now randomizes which function happens

--- kitchen/src/React-Grid-Gallery/Images.js
+++ kitchen/src/React-Grid-Gallery/Images.js
@@ -30,63 +30,77 @@ class Images extends Component {
       current: `gifs/${newCurrent}.gif`,
     })
   }
-  // handleTrack = (e) => {
-  //   let randomClass = Math.floor(Math.random() * 8)
-  //   switch (randomClass) {
-  //     case 0:
-  //       randomClass = "zero";
-  //       break;
-  //     case 1:
-  //       randomClass = "one";
-  //       break;
-  //     case 2:
-  //       randomClass = "two";
-  //       break;
-  //     case 3:
-  //       randomClass = "three";
-  //       break;
-  //     case 4:
-  //       randomClass = "four";
-  //       break;
-  //     case 5:
-  //       randomClass = "five";
-  //       break;
-  //     case 6:
-  //       randomClass = "six";
-  //       break;
-  //     case 7:
-  //       randomClass = "seven";
-  //       break;
-  //     case 8:
-  //       randomClass = "eight";
-  //   }
-  //   this.setState({
-  //     imageObject: {
-  //       url: `gifs/${this.state.current}.gif`,
-  //       x: e.nativeEvent.offsetX,
-  //       y: e.nativeEvent.offsetY,
-  //       class: randomClass
-  //     }
-  //   });
-  // }
-  // handleClick = () => {
-  //   let imageToBeRendered = this.state.imageObject
-  //   let objectToArray = [imageToBeRendered]
-  //   let newArray = this.state.renderArray.concat(objectToArray)
-  //   let newCurrent = this.state.imgArray.shift()
-  //   let updatedImgArray = this.state.imgArray
-  //   updatedImgArray.shift()
-  //   updatedImgArray.push(newCurrent)
-  //   this.setState({
-  //     renderArray: newArray,
-  //     imgArray: updatedImgArray,
-  //     current: newCurrent
-  //   })
-  // }
-  handleAudio = () => {
+  handleTrack = (e) => {
+    let randomClass = Math.floor(Math.random() * 8)
+    switch (randomClass) {
+      case 0:
+        randomClass = "zero";
+        break;
+      case 1:
+        randomClass = "one";
+        break;
+      case 2:
+        randomClass = "two";
+        break;
+      case 3:
+        randomClass = "three";
+        break;
+      case 4:
+        randomClass = "four";
+        break;
+      case 5:
+        randomClass = "five";
+        break;
+      case 6:
+        randomClass = "six";
+        break;
+      case 7:
+        randomClass = "seven";
+        break;
+      case 8:
+        randomClass = "eight";
+    }
     this.setState({
-      audio: true
+      imageObject: {
+        url: `gifs/${this.state.current}.gif`,
+        x: e.nativeEvent.offsetX,
+        y: e.nativeEvent.offsetY,
+        class: randomClass
+      }
+    });
+  }
+  handleClick = () => {
+    let randomInt = Math.floor(Math.random() * 100)
+    if (randomInt < 50) {
+      { this.imageUpload() }
+    } else {
+      { this.audioPlay() }
+    }
+  }
+  imageUpload = () => {
+    let imageToBeRendered = this.state.imageObject
+    let objectToArray = [imageToBeRendered]
+    let newArray = this.state.renderArray.concat(objectToArray)
+    let newCurrent = this.state.imgArray.shift()
+    let updatedImgArray = this.state.imgArray
+    updatedImgArray.shift()
+    updatedImgArray.push(newCurrent)
+    this.setState({
+      renderArray: newArray,
+      imgArray: updatedImgArray,
+      current: newCurrent
     })
+  }
+  audioPlay = () => {
+    if (this.state.audio === false) {
+      this.setState({
+        audio: true
+      })
+    } else {
+      this.setState({
+        audio: false
+      })
+    }
   }
   render() {
     return (
@@ -94,20 +108,19 @@ class Images extends Component {
         <Body>
           <section
             onMouseMove={this.handleTrack}
-            onClick={this.handleAudio}>
+            onClick={this.handleClick}>
             {this.state.audio ? <Audio></Audio> : <></>}
-            {/* // onClick={this.handleClick}>
-            // {this.state.renderArray.map(element =>
-            //   <img
-            //     src={element.url}
-            //     className={`${element.class}`}
-            //     style={{
-            //       position: "absolute",
-            //       top: `${element.y}px`,
-            //       left: `${element.x}px`
-            //     }}>
-            //   </img>
-            // )} */}
+            {this.state.renderArray.map(element =>
+              <img
+                src={element.url}
+                className={`${element.class}`}
+                style={{
+                  position: "absolute",
+                  top: `${element.y}px`,
+                  left: `${element.x}px`
+                }}>
+              </img>
+            )}
             <Background></Background>
           </section>
         </Body>
