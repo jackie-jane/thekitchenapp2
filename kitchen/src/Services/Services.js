a0f1ccbb0cf1bf789699dc7391d84be3a77815d7
Jacqueline-Jane Denney <jackiecdenney@gmail.com>
2021-02-03 08:30:17 -0500

    refactored background componenet. Created a services file with functions.

---
+++ kitchen/src/Services/Services.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+export const createGridArray = async (url) => {
+  try {
+    let result = await axios(url);
+    let data = result.data;
+    return data.sort(() => Math.random() - 0.5);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const iterateArray = (upcomingArray, finalArray) => {
+  let upcomingArrayCopy = upcomingArray
+  let finalArrayCopy = finalArray
+  finalArrayCopy.push(upcomingArrayCopy[0])
+  upcomingArrayCopy.shift()
+  return {
+    'upcomingArray': upcomingArrayCopy,
+    'finalArray': finalArrayCopy,
+  }
+}
