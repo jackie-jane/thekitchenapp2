2031ec9128f94dbb7ceefaee1c103840b6d548ff
Jacqueline-Jane Denney <jackiecdenney@gmail.com>
2021-02-07 10:04:12 -0500

    new audio

--- kitchen/src/Components/Audio.js
+++ kitchen/src/Components/Audio.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
+import { randomInt } from '../Services/Services'
 import Sound from 'react-sound'
 
-class Audio extends Component {
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { randomClass } from '../Services/onClickMethods';
 
+class Audio extends Component {
+  constructor(props) {
+    super(props);
+    state = {
+      audio: false,
+      fileNum: ''
+    }
+  }
+  componentDidMount() {
+    let audioFile = randomInt(7)
+    this.setState({
+      fileNum: audioFile
+    })
+  }
   render() {
     return (
       <Sound
-      url="Audio/testAudio.mp3"
-      playStatus={Sound.status.PLAYING}
-      playFromPosition={0 /* in milliseconds */}
-      onLoading={this.handleSongLoading}
-      onPlaying={this.handleSongPlaying}
-      onFinishedPlaying={this.handleSongFinishedPlaying}
-    />
+        url={`Audio/${this.state.fileNum}.mp3`}
+        playStatus={Sound.status.PLAYING}
+        playFromPosition={0 /* in milliseconds */}
+        onLoading={this.handleSongLoading}
+        onPlaying={this.handleSongPlaying}{this.handleStatePlay}
+        onFinishedPlaying={this.handleSongFinishedPlaying}
+      />
     );
   }
 }
